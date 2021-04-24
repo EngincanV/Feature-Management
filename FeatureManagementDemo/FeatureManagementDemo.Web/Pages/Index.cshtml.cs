6ab28fd3fc65fa4de66460e9e49693264d7537e6
EngincanV <enginveske@gmail.com>
2021-04-24 14:59:20 +0300

    Create sample FeatureManagement demo app

---
+++ FeatureManagementDemo/FeatureManagementDemo.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FeatureManagementDemo.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string PreviewVersionMessage { get; set; }
+        
+        private readonly IFeatureManager _featureManager;
+
+        public IndexModel(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        public void OnGet()
+        {
+            var isEnabled = _featureManager.IsEnabledAsync("IsPreviewVersion");
+            
+            PreviewVersionMessage = _featureManager.IsEnabledAsync("IsPreviewVersion").Result
+                ? "Uygulamanın preview versiyonuna ilgili linkten ulaşabilirsiniz."
+                : string.Empty;
+        }
+    }
+}
